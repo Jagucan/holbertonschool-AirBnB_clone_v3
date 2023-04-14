e06b176e25fd054ff6d28c537c8f665e5ff93a71
Jagucan <jjgc.2499@gmail.com>
2023-04-14 09:53:05 -0500

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,20 +2,20 @@
 """
 Module for the API
 """
-from flask import Flask
-from models import storage
-from api.v1.views import app_views
-
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
-
-
-@app.teardown_appcontext
-def shutdown_session():
-    """teardown method to shut down"""
-    storage.close()
 
 
 if __name__ == "__main__":
+
+    from flask import Flask
+    from models import storage
+    from api.v1.views import app_views
+
+    app = Flask(__name__)
+    app.register_blueprint(app_views)
+
+    @app.teardown_appcontext
+    def shutdown_session():
+        """ Teardown method to shut down """
+        storage.close()
+
     app.run(host="0.0.0.0", port=5000, threaded=True)
