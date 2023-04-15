504d41558fc2ff027866d4ffc2d4cca18c0c9a84
Jagucan <jjgc.2499@gmail.com>
2023-04-14 20:38:22 -0500

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,14 +4,28 @@
 if __name__ == "__main__":
     from api.v1.views import app_views
     from flask import jsonify, Flask
+    from models import storage
 
     app = Flask(__name__)
     app.register_blueprint(app_views)
 
-    @app_views.route("/status")
+    @app_views.route("/api/v1/status")
     def status_route():
         """ app view routes """
         response = jsonify({"status": "OK"})
         return response
+
+    @app_views.route("/api/v1/stats", methods=["GET"])
+    def get_stats():
+        """ Retrieves the number of each object by type """
+        stats = {
+            'amenities': storage.count('Amenity'),
+            'cities': storage.count('City'),
+            'places': storage.count('Place'),
+            'reviews': storage.count('Review'),
+            'states': storage.count('State'),
+            'users': storage.count('User')
+        }
+        return jsonify(stats)
 
     app.run(host="0.0.0.0", port=5000, debug=True)
