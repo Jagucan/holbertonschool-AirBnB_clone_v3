1d9fcda83fdd4c4cc935269cdfa4d1ed722b31eb
Jagucan <jjgc.2499@gmail.com>
2023-04-14 20:18:00 -0500

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,11 +5,9 @@ if __name__ == "__main__":
     from api.v1.views import app_views
     from flask import jsonify
 
-    app = Flask(__name__)
-    app.register_blueprint(app_views)
-
     @app_views.route("/status")
     def status_route():
         """ app view routes """
         return jsonify({"status": "OK"})
-        app.run(debug=True)
+
+    app.run(debug=True)
