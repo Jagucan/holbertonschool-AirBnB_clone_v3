80080967585444a82843d5faf2011ca47953ee60
Jagucan <jjgc.2499@gmail.com>
2023-04-14 19:49:29 -0500

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Index Module """
-from api.v1.views import app_views
 from flask import jsonify
 
 
 @app_views.route("/status")
 def status_route():
+    from api.v1.views import app_views
     """ app view routes """
     return jsonify({"status": "OK"})
