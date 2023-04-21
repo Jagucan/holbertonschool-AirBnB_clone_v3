f95830a2e22f321dcffabd76dd27558de8d12891
Jagucan <jjgc.2499@gmail.com>
2023-04-21 14:41:32 -0500

    commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""
-Module for the API
-"""
+""" Module for the API """
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
 
+app = Flask(__name__)
+app.register_blueprint(app_views)
 
-if __name__ == '__main__':
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ Closes the storage on teardown """
+    storage.close()
 
-    from flask import Flask, jsonify
-    from models import storage
-    from api.v1.views import app_views
+@app.errorhandler(404)
+def not_found(error):
+    """ Teardown method to not found """
+    return jsonify({'error': 'Not found'}), 404
 
-    app = Flask(__name__)
-    app.register_blueprint(app_views)
-
-    @app.teardown_appcontext
-    def teardown_db(exception):
-        """closes the storage on teardown"""
-        storage.close()
-
-    @app.errorhandler(404)
-    def not_found(error):
-        """ Teardown method to not found """
-        return jsonify({'error': 'Not found'}), 404
-
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', threaded=True, debug=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
 """ Index Module """
 
-if __name__ == '__main__':
+from api.v1.views import app_views
+from flask import jsonify, Flask
+from models import storage
 
-    from api.v1.views import app_views
-    from flask import jsonify, Flask
-    from models import storage
+@app_views.route('/status')
+def get_status():
+    """  """
+    return jsonify({'status': 'OK'})
 
-    @app_views.route('/api/v1/status')
-    def get_status():
-        """  """
-        return jsonify({'status': 'OK'})
-
-    @app_views.route('/api/v1/stats', methods=['GET'])
-    def get_stats():
-        """ Retrieves the number of each object by type """
-        stats = {
-            "amenities": storage.count("Amenity"),
-            "cities": storage.count("City"),
-            "places": storage.count("Place"),
-            "reviews": storage.count("Review"),
-            "states": storage.count("State"),
-            "users": storage.count("User")
-        }
-        return jsonify(stats)
-
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+@app_views.route('/stats')
+def get_stats():
+    """ Retrieves the number of each object by type """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
